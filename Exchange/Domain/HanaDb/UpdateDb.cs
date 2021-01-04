20aad0a16f74218a329f847ad5ab21e3a5d32567
nurlan3295 <nurlan3295@gmail.com>
2021-01-04 05:48:15 +0400

    Add project files.

---
+++ Exchange/Domain/HanaDb/UpdateDb.cs
@@ -0,0 +1,46 @@
+﻿using Exchange.DbConnection;
+using Exchange.Models;
+using SAPbobsCOM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exchange.Domain.HanaDb
+{
+    class UpdateDb
+    {
+        readonly Company DbConnection;
+        public UpdateDb()
+        {
+            DbConnection = HanaDbConnection.DbConnection();
+        }
+
+    public  void UpdateCurrency(ValCurs Currency)
+        {
+            SAPbobsCOM.SBObob bo = (SBObob)DbConnection.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoBridge);
+            foreach (var item in Currency.ValType[1].Valute)
+            {
+                switch (item.Code)
+                {
+                    case "USD":
+                        bo.SetCurrencyRate("USD", DateTime.Now, Convert.ToDouble(item.Value), true);
+                        break;
+                    case "DKK":
+                        bo.SetCurrencyRate("DKK", DateTime.Now, Convert.ToDouble(item.Value), true);
+                        break;
+                    case "EUR":
+                        bo.SetCurrencyRate("EUR", DateTime.Now, Convert.ToDouble(item.Value), true);
+                        break;
+                    case "GPB":
+                        bo.SetCurrencyRate("GPB", DateTime.Now, Convert.ToDouble(item.Value), true);
+                        break;
+                    case "SEK":
+                        bo.SetCurrencyRate("SEK", DateTime.Now, Convert.ToDouble(item.Value), true);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Exchange/Request/GetValute.cs
@@ -0,0 +1,46 @@
+﻿using Exchange.DbConnection;
+using Exchange.Domain.HanaDb;
+using Exchange.Models;
+using SAPbobsCOM;
+using SAPbouiCOM.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Exchange
+{
+    public  class GetValute
+    {
+       private  ValCurs Currency;
+        private UpdateDb UpdateDb;
+      public  GetValute()
+        {
+            UpdateDb = new UpdateDb();
+        }
+        public  void GetCurrency()
+        {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://www.cbar.az/currencies/03.12.2020.xml");
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream =response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(ValCurs));
+                    using (TextReader reader1 = new StringReader(reader.ReadToEnd()))
+                    {
+                        Currency = (ValCurs)serializer.Deserialize(reader1);
+                        UpdateDb.UpdateCurrency(Currency);
+                    }
+                }
+            }
+        }
+    }
+}
